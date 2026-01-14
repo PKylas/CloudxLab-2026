ef3ac6ec8c12691ba22320b8b466cf283ccb9531
Prabha Kylas <prabhakylas@Prabhas-MacBook-Air.local>
2026-01-14 21:14:42 +0530

    Session 3 Python basics commit 4

--- Base-addition.py
+++ Base-addition.py
@@ -5,7 +5,7 @@ ubase = int(input("Please enter a base number: "))
 x, y = input("Please enter any two numbers: ").split()
 
 res = []
-summed = i = carry = 0
+summed = i = carry = counter= 0
 a = list(map(int, str(x)))
 b = list(map(int, str(y)))
 
@@ -16,6 +16,7 @@ len = len(a) if a > b else len(b)
 while(i < len):
     summed = a[i] + b[i] + carry
     while(summed > 0):
+       counter+=1
        temp = summed%ubase
        res.append(temp)
        summed = summed/ubase
@@ -23,10 +24,13 @@ while(i < len):
        if(summed < 10):
            carry = summed
            break
+    if(counter == 0):
+        res.append(summed)
     i+=1
     summed = 0
 
-res.append(carry)
+if carry > 0:
+    res.append(carry)
 final_sum = res[::-1]
 
 final_sum = "".join(map(str, final_sum))
