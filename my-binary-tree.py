6f444ac5dcbf99cd41aff13821b41d860fb432e8
Prabha Kylas <prabhakylas@Prabhas-MacBook-Air.local>
2026-02-24 21:00:31 +0530

    Binary Tree Node search fix and node insertion
    Signed-off-by: Prabha Kylas <prabhakylas@Prabhas-MacBook-Air.local>

---
+++ my-binary-tree.py
@@ -0,0 +1,57 @@
+#! /usr/bin/python3
+
+import sys
+sys.setrecursionlimit(2000)
+
+class Node:
+    left = None
+    right = None
+    def __init__(self, val):
+        self.val = val
+    def printa(self):
+        if self.left:
+            self.left.printa()
+        print(self.val)
+        if self.right:
+            self.right.printa()
+    def search(self, value): # True or false
+        if value < self.val:
+            if self.left:
+                return self.left.search(value)
+            else:
+                return False
+        elif value == self.val:
+            return True
+        else:
+            if value > self.val:
+               if self.right:
+                 return self.right.search(value)
+               else:
+                return False
+    def insert(self, value):
+        if value < self.val:
+            if self.left:
+                return self.left.insert(value)
+            else:
+                self.left = Node(value)         
+        else:
+            if value > self.val:
+                if self.right:
+                    return self.right.insert(value)
+                else:
+                    self.right = Node(value)
+
+
+root = Node(10)
+root.left = Node(5)
+root.left.left = Node(1)
+root.left.right = Node(7)
+root.right = Node(16)
+root.right.left = Node(12)
+root.printa()
+a = 21
+print(f"Is {a} present in the binary tree?", root.search(a))
+if root.search(a) is False:
+    print(f"Let's insert {a}.")
+root.insert(a)
+root.printa()
